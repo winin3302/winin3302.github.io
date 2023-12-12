66a6fed2c72e1ab10cf45967a26c13267ce2cedc
Martinez Flores Jose Eduardo 5NV50 <153573488+winin3302@users.noreply.github.com>
2023-12-12 00:01:26 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyA8RriQJJtfl4MxbKtSwl1uBxmH48clyF0",
+  authDomain: "chatborrado-89c70.firebaseapp.com",
+  projectId: "chatborrado-89c70",
+  storageBucket: "chatborrado-89c70.appspot.com",
+  messagingSenderId: "1002843686378",
+  appId: "1:1002843686378:web:6d2dfed2f6e207c82a5e93",
+  measurementId: "G-2GNRVBFBK2"
+    });
