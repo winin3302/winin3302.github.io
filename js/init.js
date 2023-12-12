9bd4857e967ed3aeb1f5e4e41ea4b9f25d8e02ea
Martinez Flores Jose Eduardo 5NV50 <153573488+winin3302@users.noreply.github.com>
2023-12-12 00:08:57 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,14 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
+  authDomain: "autenfb2023-5d562.firebaseapp.com",
+  projectId: "autenfb2023-5d562",
+  storageBucket: "autenfb2023-5d562.appspot.com",
+  messagingSenderId: "1089728085495",
+  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
+  measurementId: "G-S1T5SCE6JP"
+    });
+
+    
